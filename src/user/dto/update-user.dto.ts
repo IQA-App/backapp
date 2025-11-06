59aacdb640b99933438dbe8187e6531ec35212a1
yuretspark15-Space <yuretspark15@gmail.com>
2025-11-05 21:37:42 -0600

    update-user.dto.ts

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,5 +1,46 @@
+import { Optional } from '@nestjs/common';
 import { CreateUserDto } from './create-user.dto';
 import { PartialType } from '@nestjs/mapped-types';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  Matches,
+  maxLength,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @Optional()
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  @Matches(/^(?!.*[^\P{Alphabetic}a-zA-Z])/u, {
+    message: 'Only Latin letters are allowed in the email',
+  })
+  email: string;
+
+  @Optional()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(18)
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  })
+  @Matches(/^(?!.*[^\P{Alphabetic}a-zA-Z])/u, {
+    message: 'Only Latin letters are allowed in the password',
+  })
+  @Matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])((?=.*\W)|(?=.*_))^[^ ]+$/, {
+    message:
+      'Password must have at least one uppercase letter, one lowercase letter, one number, one special character, and no spaces.',
+  })
+  password: string;
+}
