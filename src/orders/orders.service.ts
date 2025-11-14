2203f7cb1af63738b045c4d93cb971376e563888
yuretspark15-Space <yuretspark15@gmail.com>
2025-11-14 14:26:21 -0600

    createOrder

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { JwtService } from '@nestjs/jwt';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async createOrder(createOrderDto: CreateOrderDto) {
+    // const order = await this.orderRepository.findOne({
+    //   where: { title: createOrderDto.title,
+    //   customerEmail:createOrderDto.customerEmail },
+    // });
+
+
+    
     return 'This action adds a new order';
   }
 
