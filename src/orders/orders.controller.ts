3985aa50eee4d2d0b4b035fdce2d2fc12c29742e
yuretspark15-Space <yuretspark15@gmail.com>
2025-11-16 12:02:01 -0600

    orders controler and servcie

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -6,18 +6,24 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.createOrder(createOrderDto);
+  @UseGuards(JwtAuthGuard)
+  async create(@Req() req, @Body() createOrderDto: CreateOrderDto) {
+    const userId = await req.user.id; // getting userId from token
+    console.log('=== REQ ====', userId);
+    return await this.ordersService.createOrder(createOrderDto, userId);
   }
 
   @Get()
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
-import { JwtService } from '@nestjs/jwt';
+// import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -11,18 +11,33 @@ export class OrdersService {
   constructor(
     @InjectRepository(Order)
     private readonly orderRepository: Repository<Order>,
-    private readonly jwtService: JwtService,
+    // private readonly jwtService: JwtService,
   ) {}
 
-  async createOrder(createOrderDto: CreateOrderDto) {
-    // const order = await this.orderRepository.findOne({
-    //   where: { title: createOrderDto.title,
-    //   customerEmail:createOrderDto.customerEmail },
-    // });
+  async createOrder(createOrderDto: CreateOrderDto, userId: string) {
+    const existOrder = await this.orderRepository.findOne({
+      where: {
+        title: createOrderDto.title,
+        user: { id: userId }, // bcz entity order.entity  user: User;
+      },
+    });
 
+    if (existOrder) {
+      throw new BadRequestException('The order with this title already exists');
+    }
 
-    
-    return 'This action adds a new order';
+    // const order = new Order();
+    // order.title = createOrderDto.title;
+    // order.description = createOrderDto.description;
+    // order.user = { id: userId }; //using userEmail from request headers
+
+    const order = this.orderRepository.create({
+      title: createOrderDto.title,
+      description: createOrderDto.description,
+      user: { id: userId },
+    });
+
+    return await this.orderRepository.save(order);
   }
 
   findAll() {
