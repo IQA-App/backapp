0ccc9ad4bccd8fdf3272289b775674c061a5ed01
yuretspark15-Space <yuretspark15@gmail.com>
2025-11-26 09:50:39 -0600

    API-32 API emails retrieving

--- src/email-service/email-sending.service.ts
+++ src/email-service/email-sending.service.ts
@@ -3,26 +3,45 @@ import { error, info } from 'console';
 
 import * as nodemailer from 'nodemailer';
 import type { SentMessageInfo } from 'nodemailer';
+import { env } from 'process';
 
 @Injectable()
 export class EmailSendingService {
   async sendResetPassword(toEmail: string, code: string) {
     try {
+      //  we may need that in a future
+      // const transporter = nodemailer.createTransport({
+      //   host: 'smtp.ethereal.email',
+      //   port: 587,
+      //   secure: false,
+      //   pool: true, //  enable connection pooling
+      //   maxConnections: 5, // optional – defaults to 5
+      //   maxMessages: 100, // optional – defaults to 100
+      //   auth: {
+      //     user: process.env.ETHEREAL_USER,
+      //     pass: process.env.ETHEREAL_PASS,
+      //   },
+      //   tls: {
+      //     ciphers: 'SSLv3',
+      //   },
+      // });
+
       const transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
+        host: 'app.debugmail.io',
+        port: 25,
         secure: false,
         pool: true, //  enable connection pooling
         maxConnections: 5, // optional – defaults to 5
         maxMessages: 100, // optional – defaults to 100
         auth: {
-          user: process.env.ETHEREAL_USER,
-          pass: process.env.ETHEREAL_PASS,
+          user: process.env.DEBUGMAIL_USER,
+          pass: process.env.DEBUGMAIL_PASS,
         },
         tls: {
           ciphers: 'SSLv3',
         },
       });
+
       const info: SentMessageInfo = await transporter.sendMail({
         from: `IQA-Project-Two <no-replay@iqa.project-two.com>`,
         to: toEmail,
@@ -34,7 +53,8 @@ export class EmailSendingService {
       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
       const previewUrl = nodemailer.getTestMessageUrl(info);
 
-      return { 'Link with your code: ': previewUrl, messageId: info.messageId };
+      // return { 'Link with your code: ': previewUrl, messageId: info.messageId };
+      return { message: 'we sent confirmation to your email' };
     } catch (err: any) {
       console.error('EMAIL SENDING error:', err);
       throw err;
