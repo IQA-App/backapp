cdae3e15afdefa8f3dd726e24f9f604903bcb35e
yuretspark15-Space <yuretspark15@gmail.com>
2025-12-22 15:08:09 -0600

    custom trim json string

--- src/custom-decorators/custom-decorators.decorator.ts
+++ src/custom-decorators/custom-decorators.decorator.ts
@@ -12,3 +12,23 @@ export function Trim() {
     return value;
   });
 }
+
+//  removes trailing spaces from the json string
+export function TrimJsonString() {
+  const replacer = (key: string, value: any): any => {
+    // If the value is a string, return the trimmed version.
+    if (typeof value === 'string') {
+      return value.trim();
+    }
+    // Otherwise, return the original value.
+    return value;
+  };
+
+  return Transform(({ value }) => {
+    try {
+      return JSON.stringify(value, replacer);
+    } catch {
+      return value.trim();
+    }
+  });
+}
