6da0c590b007edc02ecfc7b7eb5e31d7ea1ddb4a
Yurets <yuretspark15@gmail.com>
2026-01-22 19:42:50 -0600

    API-42 try to fix app diff port (#28)

--- src/main.ts
+++ src/main.ts
@@ -4,38 +4,86 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.setGlobalPrefix('api');
+
+//   const config = new DocumentBuilder()
+//     .setTitle('Backapp API')
+//     .setDescription('The Backapp API description')
+//     .setVersion('1.0')
+//     .addBearerAuth({
+//       type: 'http',
+//       scheme: 'bearer',
+//       description: 'Use token from POST /login or POST /user',
+//     })
+//     .build();
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api/docs', app, document);
+
+//   //  trying to use custom decorators
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       transform: true,
+//       validateCustomDecorators: true,
+//       transformOptions: {
+//         // to make transform work
+//         enableImplicitConversion: true,
+//       },
+//       whitelist: true,
+//     }),
+//   );
+
+//   useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+//  option API-42 branch
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  try {
+    const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Backapp API')
-    .setDescription('The Backapp API description')
-    .setVersion('1.0')
-    .addBearerAuth({
-      type: 'http',
-      scheme: 'bearer',
-      description: 'Use token from POST /login or POST /user',
-    })
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+    app.enableCors();
 
-  //  trying to use custom decorators
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      validateCustomDecorators: true,
-      transformOptions: {
-        // to make transform work
-        enableImplicitConversion: true,
-      },
-      whitelist: true,
-    }),
-  );
+    app.setGlobalPrefix('api');
 
-  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+    const config = new DocumentBuilder()
+      .setTitle('Backapp API')
+      .setDescription('The Backapp API description')
+      .setVersion('1.0')
+      .addBearerAuth({
+        type: 'http',
+        scheme: 'bearer',
+        description: 'Use token from POST /login or POST /user',
+      })
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(3000);
+    //  trying to use custom decorators
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        validateCustomDecorators: true,
+        transformOptions: {
+          // to make transform work
+          enableImplicitConversion: true,
+        },
+        whitelist: true,
+      }),
+    );
+
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
+    const port = process.env.PORT || 3000;
+
+    await app.listen(port, '0.0.0.0');
+
+    console.log(`Application is running on port ${port}`);
+  } catch (error) {
+    console.error('Fatal error during start:', error);
+    process.exit(1);
+  }
 }
 bootstrap();
